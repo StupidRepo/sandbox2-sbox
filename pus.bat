bfe31d9c5057ab307cc84a792788b85ea592c518
flavouredtexts@icloud.com <flavouredtexts@icloud.com>
2021-11-23 02:59:46 -0800

    updated something

--- pus.bat
+++ pus.bat
@@ -1,6 +1,6 @@
-git add *
+git add * 
 git commit -m "updated something"
 git branch -M master
 git remote add origin https://github.com/StupidRepo/sandbox2-sbox.git
-git push -u origin master
+git push -u origin master --force
 pause
